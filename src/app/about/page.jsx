6803cfc9e5b97b0c56e6fd1626af511f57e0e18e
AliAais <aaisali228@gmail.com>
2024-04-10 06:44:28 -0700

    changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,35 @@
+import Image from 'next/image'
+
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex p-2 md:flex-row justify-between flex-col gap-x-6 text-white'>
+      <div className='md:w-[50%] w-[100%]'>
+        <p className='text-blue-500 text-xl font-bold mb-6'>About Agency</p>
+        <h1 className='text-5xl font-bold mb-6'>We Create digital ideas that are bigger, bolder, braver and better.</h1>
+        <p className='font-light mb-6'>We create digital ideas that are bigger, bolder, braver and better. We believe in good ideas flexibillity and precission. We&apos;re world&apos;s Our Special Team best consulting & finance solution provider. Wide range of web of software development services</p>
+        <div className='flex gap-x-6 mb-6'>
+          <div>
+            <p className='text-blue-500 text-2xl font-bold'>10 {' '}K+</p>
+            <p>Year of experience</p>
+          </div>
+          <div className=''>
+            <div>
+            <p className='text-blue-500 text-2xl font-bold'>234 {' '}K+</p>
+            <p>People reached</p>
+            </div>
+          </div>
+          <div>
+          <div>
+            <p className='text-blue-500 text-2xl font-bold'>5 {' '}K+</p>
+            <p>Services and plugins</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='md:w-[50%] mx-auto w-[90%] flex items-center justify-center'>
+        <Image src="/about.png" width={400} height={400} alt='about-section'/>
+      </div>
+    </div>
   )
 }
 
---
+++ src/app/page.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-between items-center gap-x-6">
+      <div className="text-white w-[50%] px-4">
+        <div className=" text-6xl font-extrabold space-y-4 mb-4">
+          <h1>Creative</h1>
+          <h1>Thoughts</h1>
+          <h1>Agency.</h1>
+        </div>
+        <p className="mb-4">
+          We are such a cool company even i dont know what we do lol lmao rofl
+        </p>
+        <div className="flex gap-x-4">
+          <button className="py-2 px-4 bg-blue-500 text-white rounded-md">
+            Learn More
+          </button>
+          <button className="py-2 px-4 bg-white text-black rounded-md">
+            Contact
+          </button>
+        </div>
+        <div className="w-[100%] h-[75px] relative">
+         <Image src="/brands.png" className="mt-4" layout="fill" alt="brands" />
+        </div>
+      </div>
+      <div className="w-[50%] max-h-[450]">
+        <Image src="/hero.gif" unoptimized width={450} height={450} alt="gif" />
+      </div>
+    </div>
+  );
+}
